b4f711f807f6933c4ddc352721bc42586ce6e6d0
Ovais Khanday <ovaiskhanday927@gmail.com>
2024-10-27 09:16:00 +0530

    w01_p3 complete

--- w01_p3_input_statements.c
+++ w01_p3_input_statements.c
@@ -1,1 +1,27 @@
 // Write a program that demonstrate the use of various input statements like getchar(), getch(), scanf().
+
+#include <stdio.h>
+#include <conio.h>
+
+int main(){
+
+    char ch = 'A';
+    printf("initial value of ch: %c\n", ch);
+    printf("Enter char value via getchar(): ");
+    ch = getchar();
+    printf("You entered: %c\n", ch);
+
+    fflush(stdin);
+
+    printf("Enter char value vai scanf(): ");
+    scanf("%c", &ch);
+    printf("You entered: %c\n", ch);
+
+
+    printf("Enter char via getch()\n");
+    ch = getch();
+    printf("You entered: %c\n", ch);
+    
+
+    return 0;
+}
