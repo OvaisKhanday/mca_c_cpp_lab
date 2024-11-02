14c3d5227b3f0cd5e5d19222fcee7caa05d29110
Ovais Khanday <ovaiskhanday927@gmail.com>
2024-11-02 19:34:34 +0530

    complete w02_p3

--- w02_p3_pascals_triangle.c
+++ w02_p3_pascals_triangle.c
@@ -1,1 +1,25 @@
 // Write a program to generate pascals triangle.
+
+#include <stdio.h>
+void printPascal(int n)
+{
+    for (int line = 1; line <= n; line++) {
+        for (int space = 1; space <= n - line; space++)
+            printf("  ");
+        int coef = 1;
+        for (int i = 1; i <= line; i++) {
+            printf("%4d", coef);
+            coef = coef * (line - i) / i;
+        }
+        printf("\n");
+    }
+}
+
+int main()
+{
+    int n = 5;
+    printf("Enter the size of pascal\'s triangle: ");
+    scanf("%d", &n);
+    printPascal(n);
+    return 0;
+}
