2cbb6b898421e06e286be26634cc0463a8584f51
Ovais Khanday <ovaiskhanday927@gmail.com>
2024-11-02 20:04:04 +0530

    add another approach to w01_p2

--- w01_p2_month_number_to_name.c
+++ w01_p2_month_number_to_name.c
@@ -9,47 +9,56 @@ int main()
     printf("Enter month number: ");
     scanf("%d", &monthNumber);
 
-    switch (monthNumber)
-    {
-    case 1:
-        printf("January\n");
-        break;
-    case 2:
-        printf("February\n");
-        break;
-    case 3:
-        printf("March\n");
-        break;
-    case 4:
-        printf("April\n");
-        break;
-    case 5:
-        printf("May\n");
-        break;
-    case 6:
-        printf("June\n");
-        break;
-    case 7:
-        printf("July\n");
-        break;
-    case 8:
-        printf("August\n");
-        break;
-    case 9:
-        printf("September\n");
-        break;
-    case 10:
-        printf("October\n");
-        break;
-    case 11:
-        printf("November\n");
-        break;
-    case 12:
-        printf("December\n");
-        break;
-    default:
-        printf("Not a valid month number\n");
-        break;
+    char *monthNames[12] = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+
+    if(monthNumber >= 1 && monthNumber <= 12){
+        printf("%s", monthNames[monthNumber - 1]);
+    }else {
+        printf("Not a valid Number");
     }
+
+    // Another Approach
+    // switch (monthNumber)
+    // {
+    // case 1:
+    //     printf("January\n");
+    //     break;
+    // case 2:
+    //     printf("February\n");
+    //     break;
+    // case 3:
+    //     printf("March\n");
+    //     break;
+    // case 4:
+    //     printf("April\n");
+    //     break;
+    // case 5:
+    //     printf("May\n");
+    //     break;
+    // case 6:
+    //     printf("June\n");
+    //     break;
+    // case 7:
+    //     printf("July\n");
+    //     break;
+    // case 8:
+    //     printf("August\n");
+    //     break;
+    // case 9:
+    //     printf("September\n");
+    //     break;
+    // case 10:
+    //     printf("October\n");
+    //     break;
+    // case 11:
+    //     printf("November\n");
+    //     break;
+    // case 12:
+    //     printf("December\n");
+    //     break;
+    // default:
+    //     printf("Not a valid month number\n");
+    //     break;
+    // }
     return 0;
 }
