83d4b4f19c09c72f176dfd894f2aa6ef433b1b7b
Ovais Khanday <ovaiskhanday927@gmail.com>
2024-11-07 22:11:59 +0530

    complete w06_p6

---
+++ w06_p6_vowels_consonants.c
@@ -0,0 +1,33 @@
+// Write a program to find the number of vowels and consonants in a sentence?
+
+#include <stdio.h>
+#define FALSE 0
+#define TRUE 1
+
+int isVowel(char ch){
+    char str[] = {'a', 'A', 'e', 'E', 'i', 'I', 'o', 'O', 'u', 'U'};
+    for (int i = 0; i < sizeof(str); i++){
+        if(str[i] == ch) return TRUE;
+    }
+    return FALSE;
+}
+int main(){
+    char str1[100];
+    printf("Enter a string: ");
+    gets(str1);
+
+    int vowels = 0, consonants = 0;
+
+    char *ptr = str1;
+    while(*ptr){
+        if(isVowel(*ptr))
+            vowels++;
+            else if(*ptr != ' ')
+                consonants++;
+        ptr++;
+    }
+
+    printf("Vowels: %d\nConsonants: %d\n", vowels, consonants);
+
+    return 0;
+}
