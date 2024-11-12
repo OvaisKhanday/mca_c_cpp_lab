8b8fe90da4c8bba7d62c84494e4de450c1c0f3da
Ovais Khanday <ovaiskhanday927@gmail.com>
2024-11-12 18:08:33 +0530

    change class to Student

--- w09_p4_constructors.cpp
+++ w09_p4_constructors.cpp
@@ -3,41 +3,46 @@
 #include <iostream>
 using namespace std;
 
-class Person
+class Student
 {
     string name;
     int age;
+    float marks;
 
 public:
-    Person()
+    Student()
     {
         age = 18;
+        marks = 33.0f;
     }
-    Person(string name, int age)
+    Student(string name, int age, float marks)
     {
         this->name = name;
         this->age = age;
+        this->marks = marks;
     }
-    Person(Person &p)
+    Student(Student &p)
     {
         this->age = p.age;
         this->name = p.name;
+        this->marks = p.marks;
     }
     void print()
     {
         cout << "Name: " << name << endl;
         cout << "Age: " << age << endl;
+        cout << "Marks: " << marks << endl;
     }
 };
 int main()
 {
 
-    Person p1;                       // default constructor
-    Person p2 = {"Ovais Ahmad", 23}; // parametrized
-    Person *p3 = new Person(p2);     // copy
+    Student s1;                              // default constructor
+    Student s2 = {"Ovais Ahmad", 23, 90.0f}; // parametrized
+    Student *s3 = new Student(s2);           // copy
 
-    p1.print();
-    p2.print();
-    p3->print();
+    s1.print();
+    s2.print();
+    s3->print();
     return 0;
 }
